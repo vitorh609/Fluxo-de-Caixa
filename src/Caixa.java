df2d8403c09fc37484e636042ea50490323958d3
sucesso <vitorhugoalvesbezerra88@gmail.com>
2022-06-24 21:06:48 -0300

    Correcao do bug que impedia de o usuario retornar para o menu apos selecionar nao deseja realizar uma nova venda

--- src/Caixa.java
+++ src/Caixa.java
@@ -4,6 +4,7 @@ public class Caixa {
     public static void main(String[] args) throws Exception {
         Produtos produtos = new Produtos();
         produtos.cadastrarProduto();
+        
         produtos.venderProduto();
     }
 }
